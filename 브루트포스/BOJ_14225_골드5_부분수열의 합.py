151ee7ca095f2b2484b2472da7cbbe06d26bf43e
seonkyo <tjs0466@naver.com>
2021-08-01 00:51:36 +0900

    add file

--- 브루트포스/BOJ_14225_골드5_부분수열의 합.py
+++ 브루트포스/BOJ_14225_골드5_부분수열의 합.py
@@ -1,15 +1,39 @@
+# 재귀함수 풀이 방법
 n = int(input())
 a = list(map(int, input().split()))
-c = [False] * (n*100000+10)
-for i in range(1<<n):
-    s = 0
-    for j in range(n):
-        if (i&(1<<j)):
-            s += a[j]
-    c[s] = True
-i = 1
+check = [False] * ((n*100000)+10)
+def go(i, s):
+    if i == len(a):
+        check[s] = True
+        return
+
+    go(i+1, s)
+    go(i+1, s+a[i])
+
+go(0, 0)
+
+z = 0
 while True:
-    if c[i] == False:
+    if check[z] == False:
+        print(z)
         break
-    i +=1
-print(i)
+    z +=1
+
+
+# 비트 마스크 풀이 방법
+
+# n = int(input())
+# a = list(map(int, input().split()))
+# c = [False] * (n*100000+10)
+# for i in range(1<<n):
+#     s = 0
+#     for j in range(n):
+#         if (i&(1<<j)):
+#             s += a[j]
+#     c[s] = True
+# i = 1
+# while True:
+#     if c[i] == False:
+#         break
+#     i +=1
+# print(i)
